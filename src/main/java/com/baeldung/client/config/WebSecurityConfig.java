01cda514d57d7b2e71e1ae9ce9429ae77ad595a4
brunostuessy <bruno.stuessi@gmail.com>
2024-11-18 19:19:55 +0100

    Add OidcClientInitiatedLogoutSuccessHandler to logout from keycloak as well

--- src/main/java/com/baeldung/client/config/WebSecurityConfig.java
+++ src/main/java/com/baeldung/client/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.baeldung.client.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,6 +19,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @Value("${app.security.oauth2.client.logout.redirect-uri:http://localhost:8082/oauth-client-spring}")
+    private String postLogoutRedirectUri;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,10 +36,21 @@ public class WebSecurityConfig {
             .oauth2Login(withDefaults())
             .logout(logout -> logout
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
                 .invalidateHttpSession(true)
+                //.deleteCookies()
             );
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        // Sets the location that the End-User's User Agent will be redirected to
+        // after the logout has been performed at the Provider
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri(postLogoutRedirectUri);
+
+        return oidcLogoutSuccessHandler;
     }
 
     @Bean
