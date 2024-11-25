136d98d5aed25aa23267688b94b33cd00f2062b6
brunostuessy <bruno.stuessi@gmail.com>
2024-11-25 10:30:51 +0100

    Add tomcat customizer

---
+++ src/main/java/io/bunostuessi/client/config/TomcatWebServerCustomizer.java
@@ -0,0 +1,15 @@
+package io.bunostuessi.client.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        //System.out.println(factory);
+    }
+
+}
